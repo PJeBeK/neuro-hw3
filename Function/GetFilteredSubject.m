2bed674b281a0980dd7d3c7cec3d5761b81cb4d9
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2018-05-07 13:22:23 +0430

    Fix bug

--- Function/GetFilteredSubject.m
+++ Function/GetFilteredSubject.m
@@ -1,12 +1,12 @@
 function subject = GetFilteredSubject(subject_id)
-    subject = load("s" + int2str(subject_id) + ".mat");
+    subject = load(strcat('s', int2str(subject_id), '.mat'));
     fns = fieldnames(subject);
     subject = subject.(fns{1});
 
     % Filter subject here
     bpf = BPF(1001, 0.5, 30, 256);
-    %for i = 2:9
-    %    subject.train(i,:) = FilterDFT(subject.train(i,:), bpf);
-    %    subject.test(i,:) = FilterDFT(subject.test(i,:), bpf);
-    %end
+    for i = 2:9
+        subject.train(i,:) = FilterDFT(subject.train(i,:), bpf);
+        subject.test(i,:) = FilterDFT(subject.test(i,:), bpf);
+    end
 end
